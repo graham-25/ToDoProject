7a103f7e9cda6b1561c8621cb24d26c6b5abe278
graham-25 <grbills29@googlemail.com>
2024-10-26 12:01:12 +0000

    getting front end to talk to back end

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -6,6 +8,11 @@ def home(request):
     return render(request, 'todoapp/todo.html', {})
 
 def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
     return render(request, 'todoapp/register.html', {})
 
 def loginpage(request):
